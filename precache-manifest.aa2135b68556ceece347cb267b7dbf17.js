2b2840d2d50a5ffe7f9bd19eebcf71400bccf31b
Jared Addair <addairjared@gmail.com>
2023-05-28 16:55:49 -0700

    Updates

---
+++ precache-manifest.aa2135b68556ceece347cb267b7dbf17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c26100079f74f6edf93",
+    "url": "/RoboFriends2/static/css/main.3c3e80e6.chunk.css"
+  },
+  {
+    "revision": "5c26100079f74f6edf93",
+    "url": "/RoboFriends2/static/js/main.5c261000.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/RoboFriends2/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "6bd8b6d55e231fd69044",
+    "url": "/RoboFriends2/static/css/2.26b6fda9.chunk.css"
+  },
+  {
+    "revision": "6bd8b6d55e231fd69044",
+    "url": "/RoboFriends2/static/js/2.6bd8b6d5.chunk.js"
+  },
+  {
+    "revision": "1e4ab85a9dbfa347784a",
+    "url": "/RoboFriends2/static/js/runtime~main.1e4ab85a.js"
+  },
+  {
+    "revision": "cd4216c0ebcef76bfa2afb81146d125a",
+    "url": "/RoboFriends2/index.html"
+  }
+];
